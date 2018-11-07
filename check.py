a9a22c430ada9a04a542c56d1084d443f7f8afdf
mulhern <amulhern@redhat.com>
2018-11-07 15:23:38 -0500

    Omit pylint disable invalid name
    Signed-off-by: mulhern <amulhern@redhat.com>

--- check.py
+++ check.py
@@ -6,11 +6,11 @@ import sys
 
 arg_map = {
     "src/dbus_signature_pyparsing": [
-        "--reports=no", "--disable=I", "--disable=invalid-name",
+        "--reports=no", "--disable=I",
         "--msg-template='{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}'"
     ],
     "tests": [
-        "--reports=no", "--disable=invalid-name",
+        "--reports=no",
         "--msg-template='{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}'"
     ]
 }
