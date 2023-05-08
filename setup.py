b880a0de3c5a6027d8e81ba8304ccabd0b9ee864
mulhern <amulhern@redhat.com>
2023-05-07 21:48:38 -0400

    Move setup.py contents to setup.cfg
    Signed-off-by: mulhern <amulhern@redhat.com>

--- setup.py
+++ setup.py
@@ -2,51 +2,7 @@
 Python packaging file for setup tools.
 """
 
-# isort: STDLIB
-import os
-
 # isort: THIRDPARTY
 import setuptools
 
-
-def local_file(name):
-    """
-    Function to obtain the relative path of a filename.
-    """
-    return os.path.relpath(os.path.join(os.path.dirname(__file__), name))
-
-
-with open(
-    local_file("src/dbus_signature_pyparsing/_version.py"), encoding="utf-8"
-) as o:
-    exec(o.read())  # pylint: disable=exec-used
-
-with open(local_file("README.rst"), encoding="utf-8") as o:
-    long_description = o.read()
-
-setuptools.setup(
-    name="dbus-signature-pyparsing",
-    version=__version__,  # pylint: disable=undefined-variable
-    author="Anne Mulhern",
-    author_email="amulhern@redhat.com",
-    url="https://github.com/stratis-storage/dbus-signature-pyparsing",
-    description="dbus signature parser",
-    long_description=long_description,
-    platforms=["Linux"],
-    license="Apache-2.0",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Topic :: Software Development :: Libraries",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    install_requires=["pyparsing"],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages("src"),
-)
+setuptools.setup()
